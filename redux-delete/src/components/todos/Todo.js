9bfbe566f7b9f0d4bf27892150af980a6cdd0819
Jeff Katz <jeffreyerickatz@gmail.com>
2017-02-06 12:12:49 -0500

    update delete readme

--- redux-delete/src/components/todos/Todo.js
+++ redux-delete/src/components/todos/Todo.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
 
 class Todo extends Component {
-  handleClick(){
-    
-    this.props.store.dispatch({type: 'DELETE_TODO', payload: this.props.id})
-  }
   render(){
     return (
       <li>
         {this.props.text}
-        <button onClick={this.handleClick.bind(this)}>Delete </button>
       </li>
     )
   }
--- redux-delete/src/reducers/manageTodo.js
+++ redux-delete/src/reducers/manageTodo.js
@@ -5,11 +5,6 @@ export default function manageTodo(state = {todos: []}, action){
     id++
     let todo = Object.assign({}, action.payload, {id: id})
       return {todos: state.todos.concat(todo)}
-    case 'DELETE_TODO':
-      let todos = state.todos.filter(function(todo){
-        return todo.id !== action.payload
-      })
-      return {todos: todos}
     default:
       return state;
   }
