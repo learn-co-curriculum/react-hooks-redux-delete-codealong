aa01cc96405aa1a135567a3860ae3462b6fdce44
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 08:44:10 -0500

    remove action.payload syntax

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,11 +1,14 @@
-let id = 0
-export default function manageTodo(state = {todos: []}, action){
+let id = 0;
+
+export default function manageTodo(state = {
+  todos: []
+}, action) {
   switch (action.type) {
     case 'ADD_TODO':
-    id++
-    let todo = Object.assign({}, action.payload, {id: id})
-      return {todos: state.todos.concat(todo)}
+      id++;
+      const todo = Object.assign({}, action.todo, { id: id });
+      return { todos: state.todos.concat(todo) };
     default:
       return state;
   }
-}
+};
