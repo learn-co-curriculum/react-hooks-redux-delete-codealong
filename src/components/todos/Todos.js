005fb976c2bdecad89ca03e902536c5f82139656
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 14:15:52 -0500

    remove id prop from Todos component to clarify step in code along

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -6,7 +6,7 @@ class Todos extends Component {
   render() {
 
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} id={todo.id} />
+      return <Todo text={todo.text} key={index} />
     });
 
     return(
