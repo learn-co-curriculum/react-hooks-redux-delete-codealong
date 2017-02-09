32d1150361a63d6e206c68f0cb796571458a23fc
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 08:38:26 -0500

    remove Todo access to store by default in Todos component

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -4,9 +4,9 @@ import Todo from './Todo'
 class Todos extends Component {
 
   render() {
-    
+
     const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} id={todo.id} store={this.props.store} />
+      return <Todo text={todo.text} key={index} id={todo.id} />
     });
 
     return(
